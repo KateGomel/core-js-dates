d4f2cb7f3759361d866a2ebec7e08c045e1496e2
KateGomel <ykatuay@gmail.com>
2025-03-19 19:33:45 +0300

    feat: add solutions getDayName & getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,8 +50,18 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const daysName = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const dayIndex = new Date(date).getDay();
+  return daysName[dayIndex];
 }
 
 /**
@@ -65,8 +75,12 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const nextFriday = new Date(date);
+  const currentDay = nextFriday.getDay();
+  const daysToFriday = (5 - currentDay + 7) % 7 || 7;
+  nextFriday.setDate(nextFriday.getDate() + daysToFriday);
+  return nextFriday;
 }
 
 /**
